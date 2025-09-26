6cea7a410d8ab8fd836ed223500c3e53cb8b48de
jessieakdogan <jessieakdogan@gmail.com>
2025-09-26 13:55:51 +0100

    first commit

--- client/src/utils/actions/api.ts
+++ client/src/utils/actions/api.ts
@@ -3,8 +3,4 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  withCredentials: true, // ensures cookies (session, role) are included
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
