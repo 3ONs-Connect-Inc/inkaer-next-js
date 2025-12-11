1cbca2af185dec6bf5d9c4cc289b78cd5d397a27
ogbuka-jessica <ogbukajessica@gmail.com>
2025-12-11 16:02:36 +0000

    Build: (c21dd1c) first commit

--- _next/static/chunks/4ea159a298c5661f.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/6491ac80415832a1.js",
-  "static/chunks/ac81b37a1bfe9d75.js",
-  "static/chunks/turbopack-782b0cc7d6c693df.js"
-])
---
+++ _next/static/chunks/dcd9a37ac558f137.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/c9ca1aff5d68bd81.js",
+  "static/chunks/dec23bbbe6393125.js",
+  "static/chunks/turbopack-b5079631366b8fd3.js"
+])
